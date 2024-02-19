8c14498d0f3c3ea691162696d90b4914d9d9121b
RohanKhatua <contact@rohankhatua.dev>
2024-02-19 23:45:34 +0530

    beams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import ArticleList from "@/components/article-list";
+import BackgroundBeams from "@/components/ui/background-beams";
 export default async function Home() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-16">
-      <div className="text-7xl font-light text-primary">
-        Hacker News - But Better
-      </div>
-      <ArticleList />
-    </main>
+    <div>
+      <main className="flex min-h-screen flex-col items-center p-16">
+        <div className="text-7xl font-light text-primary">
+          Hacker News - But Better
+        </div>
+        <ArticleList />
+      </main>
+      <BackgroundBeams className="-z-10"></BackgroundBeams>
+    </div>
   );
 }
--- src/components/article-list.tsx
+++ src/components/article-list.tsx
@@ -4,6 +4,7 @@ import { Button } from "./ui/button";
 import ArticleComponent from "./article-title-component";
 import Loading from "./ui/loading";
 import { useArticles } from "@/hooks/article-hook";
+import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons";
 
 export default function ArticleList() {
 
@@ -11,7 +12,7 @@ export default function ArticleList() {
 
     return (
         loading ? <Loading /> :
-            <div>
+            <div className="flex flex-col items-center">
                 <div className="flex flex-col gap-5 py-8">
                     <div className="text-center">
                         Page {page}
@@ -19,12 +20,17 @@ export default function ArticleList() {
                     {articles.map((article) => (
                         <ArticleComponent key={article.id} {...article} />
                     ))}
-                    <div className="flex flex-row gap-5 items-center justify-center">
-                        {page != 1 ? <Button onClick={() => setPage(page - 1)}>Previous Page</Button> : null}
-                        <Button onClick={() => setPage(page + 1)}>Next Page</Button>
+
+                    <div className="flex flex-row gap-5 bottom-5 justify-center">
+                        {page != 1 ? <Button variant="link" onClick={() => setPage(page - 1)}>
+                            <ArrowLeftIcon className="size-8"></ArrowLeftIcon>
+                        </Button> : null}
+                        <Button variant="link" onClick={() => setPage(page + 1)}><ArrowRightIcon className="size-8"></ArrowRightIcon></Button>
                     </div>
                 </div>
-            </div>
+
+
+            </div >
     );
 }
 
