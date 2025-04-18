f8afb2fa550d91d66a06e4bf5415c2a5c03af21e
RohanKhatua <contact@rohankhatua.dev>
2025-04-18 21:41:35 +0530

    fix: prisma runtime

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/prisma/app/generated/prisma/client";
+import { PrismaClient } from "@/prisma-client";
 
 // Create a single instance of Prisma Client
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
